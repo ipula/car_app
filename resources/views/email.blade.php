5b06c947c80e689bc6f0fb7e01fa04d7459bd25c
ipular <ipular@geniussoft.lk>
2017-08-22 10:52:20 +0530

    conformation email

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -7,8 +7,8 @@
 <h2>Login Details</h2>
 
 <div>
-    To login your details,use this<a href="http://139.59.235.246:8080">link</a>.<br/>
-    {{--<p> email : {{array($user['email'])}} </p>--}}
+    To login your details,use this<a href="http://139.59.235.246:8080"> link</a>.<br/>
+    <p> email : {{$user['email']}} </p>
     {{--<p> password : {{array($user['password'])}} </p>--}}
 </div>
 </body>
