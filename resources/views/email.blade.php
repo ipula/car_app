6bfbb28b76b413e76faf7e396ca5dfbb9d2c7e87
ipular <ipular@geniussoft.lk>
2017-08-22 10:48:05 +0530

    email blade fixing

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -8,8 +8,8 @@
 
 <div>
     To login your details,use this<a href="http://139.59.235.246:8080">link</a>.<br/>
-    <p> email : {{array($user['email'])}} </p>
-    <p> password : {{array($user['password'])}} </p>
+    {{--<p> email : {{array($user['email'])}} </p>--}}
+    {{--<p> password : {{array($user['password'])}} </p>--}}
 </div>
 </body>
 </html>
