9a73620be4d9257673d43b0625095833c2910115
ipular <ipular@geniussoft.lk>
2017-08-22 14:02:13 +0530

    set url

--- resources/views/reset_password.blade.php
+++ resources/views/reset_password.blade.php
@@ -7,7 +7,7 @@
 <h2>Password Reset</h2>
 
 <div>
-    To reset your password, complete this form.Use this <a href="http://139.59.235.246:8080/forgetPassword/".{{$token}}> link</a>.<br/>
+    To reset your password, complete this form.Use this <a href="http://localhost:4200/forgetPassword/".{{$token}}> link</a>.<br/>
 </div>
 </body>
 </html>
